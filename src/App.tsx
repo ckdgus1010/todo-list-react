46af458bc7c9f3d52a736fe065bf5c7e147151f2
ckdgus1010 <ckdgus3735@gmail.com>
2025-08-12 19:43:29 +0900

    feat: CSS 초기화를 위한 GlobalStyle.ts 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+
 function App() {
-    return null;
+    return (
+        <>
+            <GlobalStyle />
+        </>
+    );
 }
 
 export default App;
